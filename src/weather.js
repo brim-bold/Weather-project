4d42f1693620442cfbbf39a5ec5db9dbe1ae1a12
brim-bold <87397805+brim-bold@users.noreply.github.com>
2022-12-19 10:39:20 -0500

    remove console.log() function calls from javaScript and incorporate conversion functionality with forecast data

--- src/weather.js
+++ src/weather.js
@@ -15,6 +15,9 @@ let min = null;
 let max = null;
 let windSpeed = null;
 let units = "imperial";
+let forecastTemp = [1, 2, 3, 4, 5, 6];
+let forecastTempMax = [];
+let forecastTempMin = [];
 
 //code to display the lasted time the page was updated
 function updateTime() {
@@ -45,8 +48,6 @@ function formatDay(timestamp) {
 
 //code to display 6 day forecast
 function displayForecast(response) {
-  console.log(response);
-
   //set min and max temperature global variables
   min = response.data.daily[0].temperature.minimum;
   max = response.data.daily[0].temperature.maximum;
@@ -58,16 +59,17 @@ function displayForecast(response) {
   temperatureMin.innerHTML = `${Math.round(min)}`;
 
   let forecast = response.data.daily;
-  console.log(forecast);
 
   let forecastHTML = document.querySelector("#forecast");
   forecastHTML.innerHTML = null;
+  forecastTempMax = [null];
+  forecastTempMin = [null];
 
   forecast.forEach(function (forecastDay, index) {
-    if (index > 0) {
+    if (index > 0 && index < 7) {
       forecastHTML.innerHTML =
         forecastHTML.innerHTML +
-        `<div class="col-2 text-center">
+        `<div class="col-2 index${index} text-center">
               <p class="day-forecast">${formatDay(forecastDay.time)}</p>
               <img
                 src="${forecastDay.condition.icon_url}"
@@ -76,27 +78,25 @@ function displayForecast(response) {
                 class="icon-forecast"
               />
               <p class="temperature-forecast">
-                <span id="max-temp-forecast" class="max-temp">${Math.round(
-                  forecastDay.temperature.maximum
-                )}</span>
-                <span id="min-temp-forecast" class="min-temp">${Math.round(
-                  forecastDay.temperature.minimum
-                )}</span>
+                <span id="max-temp-forecast-index${index}" class="max-temp">${Math.round(
+          forecastDay.temperature.maximum
+        )}</span>
+                <span id="min-temp-forecast-index${index}" class="min-temp">${Math.round(
+          forecastDay.temperature.minimum
+        )}</span>
               </p>
             </div>`;
+      forecastTempMax.push(forecastDay.temperature.maximum);
+      forecastTempMin.push(forecastDay.temperature.minimum);
     }
   });
 }
 
 function updateForecast(coords) {
-  console.log(coords);
   let latitude = coords.latitude;
   let longitude = coords.longitude;
-  console.log(latitude);
-  console.log(longitude);
 
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=${units}`;
-  //https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}
 
   https: axios.get(apiUrl).then(displayForecast);
 }
@@ -104,8 +104,6 @@ function updateForecast(coords) {
 //code to update weather in application
 function updateWeather(response) {
   updateTime();
-
-  console.log(response);
 
   let city = document.querySelector("#city");
   city.innerHTML = `${response.data.city}`;
@@ -146,7 +144,6 @@ function citySearch() {
 
   let cityInput = document.querySelector("#city-input");
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityInput.value}&key=${apiKey}&units=${units}`;
-  //https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=${units}
 
   https: axios.get(apiUrl).then(updateWeather);
 }
@@ -206,6 +203,22 @@ function metricConversion(event) {
     windSpeed = windSpeed * 1.609;
     wind.innerHTML = `${Math.round(windSpeed * 1.609)} km/h`;
 
+    forecastTemp.forEach(function (forecastDay) {
+      let dayMin = document.querySelector(
+        `#min-temp-forecast-index${forecastDay}`
+      );
+      let newMin = ((forecastTempMin[forecastDay] - 32) * 5) / 9;
+      dayMin.innerHTML = `${Math.round(newMin)}`;
+      forecastTempMin[forecastDay] = newMin;
+
+      let dayMax = document.querySelector(
+        `#max-temp-forecast-index${forecastDay}`
+      );
+      let newMax = ((forecastTempMax[forecastDay] - 32) * 5) / 9;
+      dayMax.innerHTML = `${Math.round(newMax)}`;
+      forecastTempMax[forecastDay] = newMax;
+    });
+
     units = "metric";
 
     fahrenheit.classList.remove("active");
@@ -237,6 +250,26 @@ function imperialConversion(event) {
     let wind = document.querySelector("#wind");
     windSpeed = windSpeed / 1.609;
     wind.innerHTML = `${Math.round(windSpeed)} mph`;
+
+    forecastTemp.forEach(function (forecastDay) {
+      let dayMin = document.querySelector(
+        `#min-temp-forecast-index${forecastDay}`
+      );
+      let newMin = (forecastTempMin[forecastDay] * 9) / 5 + 32;
+      dayMin.innerHTML = `${Math.round(newMin)}`;
+      forecastTempMin[forecastDay] = newMin;
+
+      let dayMax = document.querySelector(
+        `#max-temp-forecast-index${forecastDay}`
+      );
+      let newMax = (forecastTempMax[forecastDay] * 9) / 5 + 32;
+      dayMax.innerHTML = `${Math.round(newMax)}`;
+      forecastTempMax[forecastDay] = newMax;
+
+      console.log(forecastDay);
+      console.log(forecastTempMin[forecastDay]);
+      console.log(forecastTempMax[forecastDay]);
+    });
 
     units = "imperial";
 
